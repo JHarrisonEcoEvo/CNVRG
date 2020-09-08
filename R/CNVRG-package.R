c69bee3cf9fcd69bb30bd0d0f4f0ba212b2556e1
J. G. Harrison <jgharrison1982@gmail.com>
2020-09-08 12:32:27 -0600

    minor style edits

--- R/CNVRG-package.R
+++ R/CNVRG-package.R
@@ -1,6 +1,6 @@
 #' The 'CNVRG' package.
 #'
-#' @description This package implements Dirichlet-multinomial modeling of relative abundance data using functionality provided by the Stan software. The purpose of this package is to provide a user-friendly way to interface with Stan that is suitable for those new to R and modeling. For more regarding the modeling mathematics and computational techniques see our publication in Molecular Ecology Resources titled "Dirichlet‐multinomial modelling outperforms alternatives for analysis of microbiome and other ecological count data". DOI: 10.1111/1755-0998.13128.
+#' @description This package implements Dirichlet multinomial modeling of relative abundance data using functionality provided by the 'Stan' software. The purpose of this package is to provide a user friendly way to interface with 'Stan' that is suitable for those new modelling.
 #'
 #' @docType package
 #' @name CNVRG-package
@@ -11,6 +11,6 @@
 #' @importFrom rstan sampling
 #'
 #' @references
-#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.2. https://mc-stan.org
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.2.
 #'
 NULL
